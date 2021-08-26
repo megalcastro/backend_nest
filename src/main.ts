d690216ca49519e906b012a9d703a9f3d16bbd28
megalcastro <lcastro@megatiendas.co>
2021-08-26 16:04:04 -0500

    add feature swagger document Api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Api dummy building')
+    .setDescription('This document for Api')
+    .setVersion('1.0')
+    .addTag('example')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.listen(3000);
 }
