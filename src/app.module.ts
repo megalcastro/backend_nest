9bf2ba31a2b2db1117936873609e5a9b70d5b6e0
megalcastro <lcastro@megatiendas.co>
2021-08-26 16:29:30 -0500

    add controller health

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TerminusModule } from '@nestjs/terminus';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/dummy'),AuthModule],
-  controllers: [AppController],
+  imports: [MongooseModule.forRoot('mongodb://localhost/dummy'),AuthModule,TerminusModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
