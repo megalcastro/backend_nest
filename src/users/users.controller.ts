1f799fb0ad28643c957bde5403bcda3df165bc08
megalcastro <lcastro@megatiendas.co>
2021-08-27 13:55:50 -0500

    add user JwtAuthGuard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Req, Body, Get } from '@nestjs/common';
+import { Controller, Post,Body, Get,UseGuards } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -7,6 +8,7 @@ export class UsersController {
     constructor(private userService: UsersService) { }
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     createUser(@Body() postData): string {
         this.userService.create(postData);
         return postData;
